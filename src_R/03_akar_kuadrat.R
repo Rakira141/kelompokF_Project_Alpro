3afde5b9f021cbe7a409b9868d100f6ab45d871c
Rakira141 <yudhisds51@gmail.com>
2026-06-09 21:43:04 +0700

    Add files via upload

---
+++ src_R/03_akar_kuadrat.R
@@ -0,0 +1,57 @@
+cat("===== BEFORE =====\n")
+
+# BEFORE
+a <- 1
+b <- -5
+c <- 6
+
+if (a == 0) {
+  
+  cat("Error: Nilai a tidak boleh 0 karena bukan persamaan kuadrat.\n")
+  
+} else {
+  
+  diskriminan <- b^2 - 4*a*c
+  
+  if (diskriminan < 0) {
+    
+    cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+    
+  } else {
+    
+    x1 <- (-b + sqrt(diskriminan)) / (2*a)
+    x2 <- (-b - sqrt(diskriminan)) / (2*a)
+    
+    cat(sprintf("Akar pertama (x1) = %.3f\n", x1))
+    cat(sprintf("Akar kedua (x2) = %.3f\n", x2))
+  }
+}
+
+cat("\n===== AFTER =====\n")
+
+# AFTER
+a <- 1
+b <- -5
+c <- 6
+
+if (a == 0) {
+  
+  cat("Error: Nilai a tidak boleh 0 karena bukan persamaan kuadrat.\n")
+  
+} else {
+  
+  diskriminan <- b^2 - 4*a*c
+  
+  if (diskriminan < 0) {
+    
+    cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+    
+  } else {
+    
+    x1 <- (-b + sqrt(diskriminan)) / (2*a)
+    x2 <- (-b - sqrt(diskriminan)) / (2*a)
+    
+    cat("Akar pertama (x1) =", x1, "\n")
+    cat("Akar kedua (x2) =", x2, "\n")
+  }
+}
