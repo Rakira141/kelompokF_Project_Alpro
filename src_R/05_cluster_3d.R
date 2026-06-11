fafc63d2ca7a6fd430a8a328adbb1a2b275a6085
Rakira141 <yudhisds51@gmail.com>
2026-06-11 21:24:25 +0700

    Refactor clustering code to use predefined point U
    Updated the input method for point U and added distance calculations to clusters A, B, and C.

--- src_R/05_cluster_3d.R
+++ src_R/05_cluster_3d.R
@@ -3,17 +3,12 @@ A <- c(2, 1, 3)
 B <- c(1, -4, 6)
 C <- c(-2, 3, -2)
 
+# Titik yang akan diklasifikasikan
+U <- c(1, 2, 3)
+
 # Fungsi menghitung jarak
 jarak <- function(p, q) {
   sqrt(sum((p - q)^2))
-}
-
-# Input titik U
-input <- readline("Masukkan titik U (x1 x2 x3): ")
-U <- as.numeric(strsplit(input, " ")[[1]])
-
-if(length(U) != 3){
-  stop("Input harus terdiri dari 3 angka.")
 }
 
 # Hitung jarak
@@ -22,12 +17,16 @@ dB <- jarak(U, B)
 dC <- jarak(U, C)
 
 # Tentukan cluster
-if(dA <= dB && dA <= dC){
+if (dA <= dB && dA <= dC) {
   cluster <- "A"
-} else if(dB <= dA && dB <= dC){
+} else if (dB <= dA && dB <= dC) {
   cluster <- "B"
 } else {
   cluster <- "C"
 }
 
-cat("Titik U masuk ke cluster", cluster, "\n")
+cat("Titik U =", U, "\n")
+cat("Jarak ke Cluster A =", round(dA, 3), "\n")
+cat("Jarak ke Cluster B =", round(dB, 3), "\n")
+cat("Jarak ke Cluster C =", round(dC, 3), "\n")
+cat("Titik U masuk ke Cluster", cluster, "\n")
