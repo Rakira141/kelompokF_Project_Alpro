6b7eff7990fc0ba0e9f4266a9510c8eaea6c5617
Rakira141 <yudhisds51@gmail.com>
2026-06-11 21:43:48 +0700

    Update 05_cluster_3d.R

--- src_R/05_cluster_3d.R
+++ src_R/05_cluster_3d.R
@@ -1,3 +1,44 @@
+===BEFORE===
+
+# Titik pusat cluster
+A <- c(2, 1, 3)
+B <- c(1, -4, 6)
+C <- c(-2, 3, -2)
+
+# Fungsi menghitung jarak
+jarak <- function(p, q) {
+  sqrt(sum((p - q)^2))
+}
+
+# Input titik U
+input <- readline("Masukkan titik U (x1 x2 x3): ")
+U <- as.numeric(strsplit(input, " ")[[1]])
+
+if(length(U) != 3){
+  stop("Input harus terdiri dari 3 angka.")
+}
+
+# Hitung jarak
+dA <- jarak(U, A)
+dB <- jarak(U, B)
+dC <- jarak(U, C)
+
+# Tentukan cluster
+if(dA <= dB && dA <= dC){
+  cluster <- "A"
+} else if(dB <= dA && dB <= dC){
+  cluster <- "B"
+} else {
+  cluster <- "C"
+}
+
+cat("Titik U masuk ke cluster", cluster, "\n")
+
+
+
+===AFTER===
+
+
 # Titik pusat cluster
 A <- c(2, 1, 3)
 B <- c(1, -4, 6)
