087bd1e039a859e67b21c6251143781b1b39a665
atharfikrikamaluddin22-debug <atharfikrikamaluddin22@gmail.com>
2026-06-13 18:19:19 +0700

    Add files via upload

---
+++ src_R/02_string_kalimat.R
@@ -0,0 +1,9 @@
+Kalimat1 <- "Saya tak 'kan menyerah."
+Kalimat2 <- 'Ia berkata, "Aku menyayangimu."'
+Kalimat3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+Kalimat4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+print(Kalimat1)
+print(Kalimat2)
+print(Kalimat3)
+print(Kalimat4)
